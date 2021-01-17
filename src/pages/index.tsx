eea4beac4de66e17c401aea4e115c8c678de46da
Matheus Pavanelli <mspavanelli@gmail.com>
2021-01-16 23:20:17 -0300

    :truck: refactor: move files to layout folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Layout from '../components/Layout'
-import Header from '../components/Header'
+import Layout from '../components/Layout/Layout'
+import Header from '../components/Layout/Header'
 
 const Home = () => {
   return (
